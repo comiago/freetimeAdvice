d66468bc9bfd1c56b84f9ab6d3b25f37f5a5adbd
gccg316 <giacoligioli316@gmail.com>
2023-09-23 16:35:12 +0200

    23092023
    addition of home and login components with page display via routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,26 +1,38 @@
 import { SharedService } from '../shared/shared.service';
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   siteName : string;
   isScrolled : boolean;
+  colorInverted : boolean;
+
+  @Input() inverted ?: boolean;
 
   constructor(private SharedService: SharedService){
     this.siteName = SharedService.siteName;
     this.isScrolled = false;
+    this.colorInverted = false;
+  }
+
+  ngOnInit() : void {
+    if(this.inverted == true){
+      this.colorInverted = true;
+    }
   }
 
   @HostListener('window:scroll', [])
   onWindowScroll(){
     if(window.scrollY > 100){
       this.isScrolled = true;
+      this.colorInverted = true;
     } else {
       this.isScrolled = false;
+      this.colorInverted = false;
     }
   }
 }
